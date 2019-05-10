4c4e3b99a6e4b059fc2e262c7cc2a8e8e40ef559
Marcelo Freire <marcelo.freire.silva@gmail.com>
2019-05-10 01:01:01 -0300

    Alterando o componente para arrow function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+const App = () => {
+  return <h1>Hello</h1>;
 }
 
 export default App;
