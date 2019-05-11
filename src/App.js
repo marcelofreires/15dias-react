342840417cc126f76ce293092e178512f0f97e04
Marcelo Freire <marcelo.freire.silva@gmail.com>
2019-05-10 22:17:46 -0300

    Criado o componente squareCollab

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import SquareCollab from './components/SquareCollab';
 
-const App = () => (
-  <SquareCollab />
-);
+const App = () => <SquareCollab />;
 
 export default App;
