e5589eb3240dafe90495134c88e432ea91736e7d
Marcelo Freire <marcelo.freire.silva@gmail.com>
2019-05-16 21:55:45 -0300

    Feito o desafio básico do dia 04 (criar component do histórico)

--- src/components/GameHistory/index.js
+++ src/components/GameHistory/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './styles.css'
 
-const GameHistory = () => <button className="game-history">history button</button>;
+const GameHistory = () => (
+  <ol>
+    <li className="game-history">X na 1x1</li>
+  </ol>
+);
 
 export default GameHistory;
